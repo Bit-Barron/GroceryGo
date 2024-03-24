59ee1d94a0e42317bb1fd86bc3ae9808e53c138c
bit-barron <1bit.baron@gmail.com>
2024-03-24 20:16:34 +0100

    clean up ultimate datatable

--- src/components/pages/Dashboard/Dashboard.tsx
+++ src/components/pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import BarChart from "@/components/elements/BarChart";
+import { TopCards } from "@/components/elements/TopCards";
+import RecentOrders from "@/components/elements/RecentOrders";
 
 interface DashboardProps {}
 
 export const Dashboard: React.FC<DashboardProps> = ({}) => {
   return (
     <div>
-      <div></div>
+      <main className="min-h-screen bg-slate-100">
+        <TopCards />
+        <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-3">
+          <BarChart />
+          <RecentOrders />
+        </div>
+      </main>
     </div>
   );
 };
--- src/components/pages/products/productupsert.tsx
+++ src/components/pages/products/productupsert.tsx
@@ -1,16 +1,10 @@
-import { Button } from "@/components/ui/button";
 import { Toaster, toast } from "sonner";
-import { Input } from "@/components/ui/input";
 import axios from "axios";
 import { AdminProductsStore } from "@/store/admin/AdminProducts";
 import React, { FormEvent } from "react";
 import { useSnapshot } from "valtio";
-import { AiOutlineSave } from "react-icons/ai";
-import { GiCancel } from "react-icons/gi";
-import { MdSubtitles } from "react-icons/md";
-import { MdDescription } from "react-icons/md";
-import { IoIosPricetags } from "react-icons/io";
 import cookie from "cookie";
+import { DataTable } from "@/components/elements/products/Table";
 
 interface ProductsProps {}
 
@@ -44,57 +38,15 @@ export const Products: React.FC<ProductsProps> = ({}) => {
     }
   };
 
-  const buttonActions = (
-    <div className="flex justify-end space-x-5 text-white">
-      <Button type="button" Icon={GiCancel}>
-        Cancel
-      </Button>
-      <Button type="submit" Icon={AiOutlineSave}>
-        Save
-      </Button>
-    </div>
-  );
-
   return (
     <form onSubmit={(e) => createProduct(e)}>
       <Toaster />
-      <div className="p-5 rounded-lg mt-10 bg-slate-100 ml-10 mr-10">
+      <div className="p-5 rounded-lg mt-10  ml-10 mr-10">
         <h1 className="font-bold text-black text-xl">Products</h1>
       </div>
 
-      <div className="p-5 rounded-lg mt-10 bg-slate-100 ml-10 mr-10 space-y-10">
-        <Input
-          onChange={(e) => productStore.setTitle(e.target.value)}
-          placeholder="Product Title"
-          value={productStore.title}
-          Icon={MdSubtitles}
-          required
-        />
-
-        <Input
-          onChange={(e) => productStore.setDescription(e.target.value)}
-          placeholder="Product Description"
-          value={productStore.description}
-          Icon={MdDescription}
-          required
-        />
-
-        <Input
-          onChange={(e) => productStore.setSmallDescription(e.target.value)}
-          placeholder="Product Small Description"
-          value={productStore.smallDescription}
-          Icon={MdDescription}
-          required
-        />
-
-        <Input
-          onChange={(e) => productStore.setPrice(e.target.value as any)}
-          placeholder="Product Price"
-          type="number"
-          Icon={IoIosPricetags}
-          required
-        />
-        <div className="pt-5">{buttonActions}</div>
+      <div className="p-5 rounded-lg ml-10 mr-10 space-y-10">
+        <DataTable />
       </div>
     </form>
   );
