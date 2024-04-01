57721d9d909d6c4e44a049ca7d84b4232720aff8
Bit-Barron <1bit.baron@gmail.com>
2024-04-01 05:06:57 +0200

    clean up

--- src/components/pages/Dashboard/Dashboard.tsx
+++ src/components/pages/Dashboard/Dashboard.tsx
@@ -1,33 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AdminCard } from "@/components/elements/card";
 import { CategoriesProps, ProductsProps } from "@/types/interface";
 import { AdminChart } from "@/components/elements/chart";
 import { AdminOrders } from "@/components/elements/order";
+import { useSnapshot } from "valtio";
+import { AdminCategoryStore } from "@/store/admin/AdminCategory";
+import { AdminProductsStore } from "@/store/admin/AdminProducts";
+import { AdminQrCodeStore } from "@/store/admin/AdminQrCode";
 
 interface DashboardProps {}
 
 export const Dashboard: React.FC<DashboardProps> = ({}) => {
-  const [products, setProducts] = useState<ProductsProps[]>([]);
-  const [category, setCategory] = useState<CategoriesProps[]>([]);
+  const categoryStore = useSnapshot(AdminCategoryStore);
+  const productStore = useSnapshot(AdminProductsStore);
+  const qrCodeStore = useSnapshot(AdminQrCodeStore);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      categoryStore.getCategoriesById();
+      productStore.getProductById();
+      qrCodeStore.getQrCodeById();
+    };
+    fetchData();
+  }, [categoryStore, productStore, qrCodeStore]);
 
   return (
     <>
       <div className="md:flex gap-10 p-4">
         <AdminCard
-          amount={products.length}
+          amount={productStore.product.length}
           description="Products"
-          percentage={products.length}
+          percentage={productStore.product.length}
         />
         <AdminCard
-          amount={category.length}
+          amount={categoryStore.category.length}
           description="Categories"
-          percentage={category.length}
+          percentage={categoryStore.category.length}
         />
 
         <AdminCard
-          amount={category.length}
-          description="Categories"
-          percentage={category.length}
+          amount={qrCodeStore.qrCode.length}
+          description="Qr Codes"
+          percentage={qrCodeStore.qrCode.length}
         />
       </div>
       <div className="md:flex gap-5 p-4">
